a5ea599b0e72f615becadc0783b73115518d37b4
aavassigdel <aavassigdel14@gmail.com>
2026-02-22 14:27:56 +0545

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ const App = () => {
 
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup/>} />
+        <Route path="/Todoapp" element={<Todoapp />} />
         
       </Routes>
       
